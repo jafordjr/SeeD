c37360d60b180a1d14dc0ace913a66b17d014212
David Burns <davidburns612@gmail.com>
2019-03-18 14:39:10 -0700

    Update app.js

--- DocHunt/app.js
+++ DocHunt/app.js
@@ -1,4 +1,4 @@
-﻿'use strict';
+'use strict';
 // Packages
 var debug = require('debug');
 var express = require('express');
@@ -8,24 +8,47 @@ var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
 var session = require('express-session');
-var mysql = require('mysql');
+var http = require('http');
+var formidable = require('formidable'); //for uploading documents
+var fs = require('fs');
+var MongoClient = require('mongodb').MongoClient;
+var url = "mongodb://localhost:27017/mydb";
 
 // Indicate server is started - Useful for commandline
 console.log("Nodejs Server started!");
 
-// Connection information for sql database
-var con = mysql.createConnection({
-    host: "sql9.freemysqlhosting.net",
-    user: "sql9279950",
-    password: "AHGNlNK3kj",
-    database: "sql9279950"
-});
+//shitty function for uploading a doc and saving to local folder, no integration with mongoDB yet 
+http.createServer(function (req, res) {
+  if (req.url == '/fileupload') {
+    var form = new formidable.IncomingForm();
+    form.parse(req, function (err, fields, files) {
+      var oldpath = files.filetoupload.path;
+      //change this ish to whatever your filepath is
+      var newpath = '/home/david/Desktop/SeeD-master/testfiles/' + files.filetoupload.name;
+      var dbValue = [newpath, files.filetoupload.name];
+      fs.rename(oldpath, newpath, function (err) {
+        if (err) throw err;
 
-// Attempting to connect to sql database
-con.connect(function (err) {
-    if (err) throw err;
-    console.log("Sql Connected!");
-});
+        res.write('File uploaded and moved!');
+        res.end();
+      });
+ });
+  } else {
+    res.writeHead(200, {'Content-Type': 'text/html'});
+    res.write('<form action="fileupload" method="post" enctype="multipart/form-data">');
+    res.write('<input type="file" name="filetoupload"><br>');
+    res.write('<input type="submit">');
+    res.write('</form>');
+    return res.end();
+  }
+}).listen(8080); 
+
+MongoClient.connect(url, function(err, db) {
+  if (err) throw err;
+  var dbo = db.db("mydb");
+    db.close();
+}); 
+
 
 // Routing for Users and Individuals
 var routes = require('./routes/index');
